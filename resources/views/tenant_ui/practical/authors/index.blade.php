20f0badddf88702d50c154a14200511c6af190b1
FallSteph <2301103366@student.buksu.edu.ph>
2026-04-28 04:26:20 +0800

    Try practical

---
+++ resources/views/tenant_ui/practical/authors/index.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="p-6">
+        <b>This is Author Index page</b>
+    </div>
+</x-app-layout>
---
+++ resources/views/tenant_ui/practical/books/index.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="p-6">
+        <b>This is Book Index page</b>
+    </div>
+</x-app-layout>
